56e7622d53b2120151a702db6e8551b41b277f17
BritsolArcher <yimingqu256@gmail.com>
2023-12-13 12:02:37 +0000

    Update CoffeeShop class

--- coffee_shop.py
+++ coffee_shop.py
@@ -5,20 +5,47 @@
 
 from barista import Barista
 from pantry import Pantry
+from cash_status import CashStatus
+from data_load import DataLoad
 
 
 class CoffeeShop:
     def __init__(self):
         self.__baristas = {}
+        self.__specialists = {  # Barista who has a speciality will be added
+            "Expresso": set(),  # Specialities are keys, while baristas' names are values
+            "Americano": set(),
+            "Filter": set(),
+            "Macchiatto": set(),
+            "Flat White": set(),
+            "Latte": set()
+        }
         self.__pantry = Pantry()
-        self.__cash = 10000
-        self.__specialists = {  # Baristas who has a speciality will be added
-            "Expresso": [],
-            "Americano": [],
-            "Filter": [],
-            "Macchiatto": [],
-            "Flat White": [],
-            "Latte": []
+        self.__cash_status = CashStatus()
+
+        self.__max_demand = DataLoad()("demand", "Coffee Types",
+                                       "Monthly Demand")
+        self.__demand = self.__ingredients_consumption_rate = {
+            "Expresso": 0,
+            "Americano": 0,
+            "Filter": 0,
+            "Macchiatto": 0,
+            "Flat White": 0,
+            "Latte": 0
+        }
+        self.__ingredients_consumption = {
+            "Milk": 0,
+            "Beans": 0,
+            "Spices": 0
+        }
+
+        self.__ingredients_consumption_rate = {
+            "Expresso": {"Milk": 0, "Beans": 8, "Spices": 0},
+            "Americano": {"Milk": 0, "Beans": 6, "Spices": 0},
+            "Filter": {"Milk": 0, "Beans": 4, "Spices": 0},
+            "Macchiatto": {"Milk": 100, "Beans": 8, "Spices": 2},
+            "Flat White": {"Milk": 200, "Beans": 8, "Spices": 1},
+            "Latte": {"Milk": 300, "Beans": 8, "Spices": 3}
         }
 
     def add_baristas(self, numbers: int, names: list,  specialities: list):
@@ -28,11 +55,15 @@ class CoffeeShop:
                 self.__baristas[name] = Barista(name, speciality, is_special=is_special)
                 print(f"Barista {name} has been employed.")
                 if is_special:
+                    self.__specialists[speciality].add(name)
                     print(f"{name} specialises in {speciality}")
                 else:
                     print(f"{name} doesn't have a speciality")
 
-    def remove_baristas(self, numbers: int, *names):
+        else:
+            pass
+
+    def remove_baristas(self, *names):
         for name in names:
             del self.__baristas[name]
 
